c6c024a773d52ca6d47b61ddaf7ea13785a142a0
程序猿讲故事 <javacodestory@gmail.com>
2019-10-14 16:13:36 +0800

    添加注释

--- research-eureka-client-portal/src/main/java/tech/codestory/research/eureka/portal/ResearchEurekaClientPortalApplication.java
+++ research-eureka-client-portal/src/main/java/tech/codestory/research/eureka/portal/ResearchEurekaClientPortalApplication.java
@@ -3,6 +3,10 @@ package tech.codestory.research.eureka.portal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 测试 Eureka 注册服务的消费者，会调用注册到 eureka 的服务
+ * @author junyongliao 
+ */
 @SpringBootApplication
 public class ResearchEurekaClientPortalApplication {
 
